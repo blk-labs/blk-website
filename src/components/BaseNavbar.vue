77dff24eeada2bb3502c6aa3cff5debe40bc2a09
Egwuchukwu Stephen Diala <104213881+dialastephen@users.noreply.github.com>
2022-06-18 20:03:32 +0100

    feat: created navbar component

---
+++ src/components/BaseNavbar.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <nav>
+        <div class="container mx-auto px-4 lg:px-0 pt-8">
+            <div class="flex justify-between items-center">
+                <img alt="BLK" src="./../assets/logo.png" class="w-24 h-24" />
+                <BaseButton type="button" class="text-3xl text-black rounded-full p-2.5 bg-white drop-shadow-xl">
+                    <BaseIcon icon="ci:menu-duo" />
+                </BaseButton>
+            </div>
+        </div>
+    </nav>
+</template>
