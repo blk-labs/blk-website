2daf9f22c0d1f1f66659e44d7e9c6ac02e08c676
Egwuchukwu Stephen Diala <104213881+dialastephen@users.noreply.github.com>
2022-06-18 15:59:44 +0100

    feat: implemented global component import

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,16 @@ import './index.css'
 
 const app = createApp(App)
 app.use(router)
+
+//global component registration
+const components = import.meta.globEager('./components/*.vue')
+Object.entries(components).forEach(([path, definition]) => {
+  // Get name of component, based on filename
+  // "./components/Fruits.vue" will become "Fruits"
+  const componentName = path.split('/').pop()!.replace(/\.\w+$/, '')
+
+  // Register component on this Vue instance
+  app.component(componentName, definition.default)
+})
+
 app.mount('#app')
