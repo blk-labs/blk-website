4de19e3d346557733c81396a2106d49830ce9067
Egwuchukwu Stephen Diala <104213881+dialastephen@users.noreply.github.com>
2022-06-22 13:07:29 +0100

    feat: added blk logo

--- src/components/BaseNavbar.vue
+++ src/components/BaseNavbar.vue
@@ -7,7 +7,7 @@ import BaseIcon from './BaseIcon.vue';
     <nav>
         <div class="container mx-auto px-4 lg:px-0 pt-8">
             <div class="flex justify-between items-center">
-                <img alt="BLK" src="./../assets/logo.png" class="w-24 h-24" />
+                <img alt="BLK" src="https://blklabs.co/static/media/blk-logoDark.13e50d81.png" class="w-24 h-24" />
                 <BaseButton type="button" class="text-3xl text-black rounded-full p-2.5 bg-white drop-shadow-xl">
                     <BaseIcon icon="heroicons-outline:menu-alt-4" />
                 </BaseButton>
