1e3d06de8f703302510108ad361fb70c153e55f5
Egwuchukwu Stephen Diala <104213881+dialastephen@users.noreply.github.com>
2022-06-22 13:42:10 +0100

    feat: added font

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,13 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
+  <BaseNavbar/>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Inter', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
