ffcdce4c1267c769ecef1be0f43264e42b8fa846
Egwuchukwu Stephen Diala <104213881+dialastephen@users.noreply.github.com>
2022-06-22 13:42:27 +0100

    feat: created work component

---
+++ src/components/BaseWork.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import BaseButton from './BaseButton.vue';
+
+interface WorkProps {
+    image: string
+    title: string
+    text: string
+}
+
+const workProps = defineProps<WorkProps>()
+</script>
+
+<template>
+    <div class="w-fit grid gap-5 max-w-2xl">
+        <img :src="workProps.image" class="object-cover object-center w-full h-[440px]">
+        <div class="grid gap-5 text-left">
+            <div class="grid gap-2.5">
+                <h1 class="text-2xl lg:text-4xl font-bold text-neutral-600">{{ workProps.title }}</h1>
+                <p class="text-base lg:text-2xl font-normal text-[#776F8D]">{{ workProps.text }}</p>
+            </div>
+            <BaseButton btnType="outline" class="uppercase w-fit py-4 px-5">View project</BaseButton>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
