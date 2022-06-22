743c87f0f452bcda99e27948da22b47ea218736f
Egwuchukwu Stephen Diala <104213881+dialastephen@users.noreply.github.com>
2022-06-22 13:42:44 +0100

    feat: modified navbar

--- src/components/BaseNavbar.vue
+++ src/components/BaseNavbar.vue
@@ -8,8 +8,8 @@ import BaseIcon from './BaseIcon.vue';
         <div class="container mx-auto px-4 lg:px-0 pt-8">
             <div class="flex justify-between items-center">
                 <img alt="BLK" src="https://blklabs.co/static/media/blk-logoDark.13e50d81.png" class="w-24 h-24" />
-                <BaseButton type="button" class="text-3xl text-black rounded-full p-2.5 bg-white drop-shadow-xl">
-                    <BaseIcon icon="heroicons-outline:menu-alt-4" />
+                <BaseButton type="button" class="rounded-full p-4 bg-white drop-shadow-xl border-none">
+                    <BaseIcon icon="heroicons-outline:menu-alt-4" class="text-2xl text-black" />
                 </BaseButton>
             </div>
         </div>
